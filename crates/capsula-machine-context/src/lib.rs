66606877c66c600e92cc7c9473bb21a2b5cb61bc
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-09-21 18:15:05 +0900

    Add machine context

---
+++ crates/capsula-machine-context/src/lib.rs
@@ -0,0 +1,99 @@
+mod config;
+
+use capsula_core::captured::Captured;
+use capsula_core::context::{Context, ContextFactory, RuntimeParams};
+use capsula_core::error::CoreResult;
+use config::MachineContextFactory;
+use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
+
+pub const KEY: &str = "machine";
+
+#[derive(Debug, Default)]
+pub struct MachineContext;
+
+#[derive(Debug)]
+pub struct CpuInfo {
+    name: String,
+    vender_id: String,
+    brand: String,
+    frequency_mhz: u64,
+}
+
+#[derive(Debug)]
+pub struct MachineCaptured {
+    pub os: String,
+    pub os_version: String,
+    pub kernel_version: String,
+    pub architecture: String,
+    pub cpus: Vec<CpuInfo>,
+    // pub cpu_cores: usize,
+    pub total_memory: usize,
+    // pub user: String,
+    pub hostname: String,
+}
+
+impl Context for MachineContext {
+    type Output = MachineCaptured;
+
+    fn run(&self, _params: &RuntimeParams) -> CoreResult<Self::Output> {
+        let os = System::name().unwrap_or("Unknown".to_string());
+        let os_version = System::os_version().unwrap_or("Unknown".to_string());
+        let kernel_version = System::kernel_version().unwrap_or("Unknown".to_string());
+        let architecture = std::env::consts::ARCH.to_string();
+
+        let system = System::new_with_specifics(
+            RefreshKind::nothing()
+                .with_cpu(CpuRefreshKind::nothing().with_frequency())
+                .with_memory(MemoryRefreshKind::nothing().with_ram()),
+        );
+        let cpus = system
+            .cpus()
+            .iter()
+            .map(|cpu| CpuInfo {
+                name: cpu.name().to_string(),
+                vender_id: cpu.vendor_id().to_string(),
+                brand: cpu.brand().to_string(),
+                frequency_mhz: cpu.frequency(),
+            })
+            .collect::<Vec<_>>();
+
+        let total_memory = system.total_memory();
+        let hostname = System::host_name().unwrap_or("Unknown".to_string());
+
+        Ok(MachineCaptured {
+            os,
+            os_version,
+            kernel_version,
+            architecture,
+            cpus,
+            total_memory: total_memory as usize,
+            hostname: hostname,
+        })
+    }
+}
+
+impl Captured for MachineCaptured {
+    fn to_json(&self) -> serde_json::Value {
+        serde_json::json!({
+            "type": KEY,
+            "os": self.os,
+            "os_version": self.os_version,
+            "kernel_version": self.kernel_version,
+            "architecture": self.architecture,
+            "cpus": self.cpus.iter().map(|cpu| {
+                serde_json::json!({
+                    "name": cpu.name,
+                    "vender_id": cpu.vender_id,
+                    "brand": cpu.brand,
+                    "frequency_mhz": cpu.frequency_mhz,
+                })
+            }).collect::<Vec<_>>(),
+            "total_memory": self.total_memory,
+            "hostname": self.hostname,
+        })
+    }
+}
+
+pub fn create_factory() -> Box<dyn ContextFactory> {
+    Box::new(MachineContextFactory)
+}
