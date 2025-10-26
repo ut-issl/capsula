333e5b31d86150a6ad31f9af462824227efa1f8a
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-10-26 22:39:56 +0900

    Remove watchers

--- crates/capsula-config/src/lib.rs
+++ crates/capsula-config/src/lib.rs
@@ -78,8 +78,6 @@ impl<'de> Deserialize<'de> for VaultConfig {
 pub struct PhaseConfig {
     #[serde(default)]
     pub pre: PrePhaseConfig,
-    #[serde(rename = "in", default)]
-    pub in_phase: InPhaseConfig,
     #[serde(default)]
     pub post: PostPhaseConfig,
 }
@@ -91,17 +89,9 @@ pub struct HookPhaseConfig {
     pub hooks: Vec<HookEnvelope>,
 }
 
-/// A phase configuration that contains watchers
-#[derive(Deserialize, Debug, Clone, Default)]
-pub struct WatcherPhaseConfig {
-    #[serde(default)]
-    pub watchers: Vec<WatcherSpec>,
-}
-
 // Type aliases for semantic clarity
 pub type PrePhaseConfig = HookPhaseConfig;
 pub type PostPhaseConfig = HookPhaseConfig;
-pub type InPhaseConfig = WatcherPhaseConfig;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct HookEnvelope {
@@ -109,15 +99,6 @@ pub struct HookEnvelope {
     #[serde(flatten)]
     pub rest: serde_json::Value,
 }
-
-#[derive(Deserialize, Debug, Clone)]
-#[serde(tag = "type", rename_all = "lowercase")]
-pub enum WatcherSpec {
-    Time(TimeWatcherSpec),
-}
-
-#[derive(Deserialize, Debug, Clone, Default)]
-pub struct TimeWatcherSpec {}
 
 impl CapsulaConfig {
     pub fn from_toml_str(content: &str) -> ConfigResult<Self> {
